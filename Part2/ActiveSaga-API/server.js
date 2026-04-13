b845ed6b5eddec2207ded32f576f99ed257af526
sashabel1 <alexandra.belkind@e.braude.ac.il>
2026-04-13 15:41:20 +0300

    add login and register logic to unity

--- Part2/ActiveSaga-API/server.js
+++ Part2/ActiveSaga-API/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const mongoURI = process.env.MONGO_URI;
